d68b899bbcc94b0d00a34205b73b75a9438fa26f
Kira Loo <youhavebeenkiraed@gmail.com>
2017-10-14 01:06:23 -0700

    initial butterknife set up

--- app/src/main/java/com/example/kira/kiraworks/MainActivity.java
+++ app/src/main/java/com/example/kira/kiraworks/MainActivity.java
@@ -1,25 +1,40 @@
 package com.example.kira.kiraworks;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private Button mAddFriendButton;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.findAddFriendButton) Button mAddFriendButton;
+    @Bind(R.id.findViewListButton) Button mViewListButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAddFriendButton = (Button) findViewById(R.id.findAddFriendButton);
-        mAddFriendButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(MainActivity.this, "New Friend Added", Toast.LENGTH_LONG).show();
-                }
-            });
+        ButterKnife.bind(this);
+
+        mViewListButton.setOnClickListener(this);
+        mAddFriendButton.setOnClickListener(this);
+
+    }
+        @Override
+            public void onClick(View v) {
+            if(v == mViewListButton) {
+                Intent intentView = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intentView);
+            }
+            else if(v == mAddFriendButton) {
+                Toast.makeText(MainActivity.this, "New Friend Added", Toast.LENGTH_LONG).show();
+            }
     }
 }
