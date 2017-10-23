39f15d114d118f116efb2aef826cdf613ebbed0a
kira <kizxm@yahoo.com>
2017-10-23 15:25:25 -0700

    view on click listener added

--- app/src/main/java/com/example/kira/kiraworks/MainActivity.java
+++ app/src/main/java/com/example/kira/kiraworks/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Bind(R.id.findViewListButton) Button mFindViewListButton;
     @Bind(R.id.numberEditText) EditText mNumberText;
@@ -29,15 +29,17 @@ public class MainActivity extends AppCompatActivity {
         Typeface lieFont = Typeface.createFromAsset(getAssets(), "fonts/lie.ttf");
         mAppNameTextView.setTypeface(lieFont);
 
-        mFindViewListButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String number = mNumberText.getText().toString();
-                    Intent intent = new Intent(MainActivity.this, ListActivity.class);
-                    intent.putExtra("number", number);
-                    startActivity(intent);
-                }
-            });
+        mFindViewListButton.setOnClickListener(this);
+    }
 
+        @Override
+            public void onClick(View v) {
+            if (v == mFindViewListButton) {
+                String number = mNumberText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                intent.putExtra("number", number);
+                startActivity(intent);
+        }
     }
 }
+
