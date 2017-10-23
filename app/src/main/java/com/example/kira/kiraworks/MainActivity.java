3388f0e1d12c2bdf64ffbce5a675152124877049
kira <kizxm@yahoo.com>
2017-10-23 13:10:08 -0700

    custom font added

--- app/src/main/java/com/example/kira/kiraworks/MainActivity.java
+++ app/src/main/java/com/example/kira/kiraworks/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.kira.kiraworks;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mViewListButton;
     private EditText mNumberText;
+    private TextView mAppNameTextView;
 
 
     @Override
@@ -20,9 +23,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mNumberText = (EditText) findViewById(R.id.locationEditText);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface lieFont = Typeface.createFromAsset(getAssets(), "fonts/lie.ttf");
+        mAppNameTextView.setTypeface(lieFont);
+
         mViewListButton = (Button) findViewById(R.id.findViewListButton);
         mViewListButton.setOnClickListener(new View.OnClickListener() {
-
                 @Override
                 public void onClick(View v) {
                     String number = mNumberText.getText().toString();
